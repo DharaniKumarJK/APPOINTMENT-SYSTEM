10a466919d9bb47da4f953cc528aeb05dc34a54b
DharaniKumarJK <dharanikumarjk@gmail.com>
2026-03-25 10:03:11 +0530

    Initial commit of Appointment Management System

---
+++ src/main/java/com/example/appointmnentmanagementsystem/AppointmnentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmnentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmnentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmnentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appointmnentmanagementsystem/AppointmnentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmnentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmnentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
